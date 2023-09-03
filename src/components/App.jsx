f27fac8dbbaeeaf7b1d5f9ed373a6b2e2e164890
Andrii Kurshatsov <andreswebit@ukr.net>
2023-09-03 17:53:48 +0300

    Update css styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from '../json/user.json';
-import data from '../json/data.json';
-import friends from '../json/friends.json';
-import transactions from '../json/transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
